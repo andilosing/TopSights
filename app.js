270ab74d27534bb36eaec2ce534dcf32e9a3af89
andilosinge <andreas.losing@gmx.de>
2022-09-15 12:19:01 +0200

    Created comments and faq page.

--- app.js
+++ app.js
@@ -24,6 +24,24 @@ app.get('/sights', function(request, response){
     response.render('sights.hbs', model)
 })
 
+app.get('/faq', function(request, response){
+
+    const model = {
+        faq: data.faq
+    }
+    
+    response.render('faq.hbs', model)
+})
+
+app.get('/comments', function(request, response){
+
+    const model = {
+        comments: data.comments
+    }
+    
+    response.render('comments.hbs', model)
+})
+
 app.get("/sights/:id", function(request, response){
 
 const id = request.params.id
--- data.js
+++ data.js
@@ -1,14 +1,68 @@
 exports.sights = [{
     id: 1, 
     name: "Lake of Konstanz",
-    location: "Konstanz" 
+    location: "Konstanz" ,
+    info: "das ist ein beispiel text. hier kann alles stehen. beschreibung etc. xxxx sjf erfjergner ekjg regwghwrth wrtgwthwrth wrthwtrhrwth"
 },
 {
     id: 2, 
     name: "Brandenburger Tor",
-    location: "Berlin" 
+    location: "Berlin" ,
+    info: "das ist ein beispiel text. hier kann alles stehen. beschreibung etc. xxxx sjf erfjergner ekjg regwghwrth wrtgwthwrth wrthwtrhrwth"
+    
 },{
     id: 3, 
     name: "Castle Neuschwanstein",
-    location: "Schwangau"
+    location: "Schwangau", 
+    info: "das ist ein beispiel text. hier kann alles stehen. beschreibung etc. xxxx sjf erfjergner ekjg regwghwrth wrtgwthwrth wrthwtrhrwth"
+},
+{
+    id: 4, 
+    name: "Sight 4",
+    location: "Berlin" ,
+    info: "das ist ein beispiel text. hier kann alles stehen. beschreibung etc. xxxx sjf erfjergner ekjg regwghwrth wrtgwthwrth wrthwtrhrwth"
+    
+},
+{
+    id: 5, 
+    name: "Sight 5",
+    location: "Berlin" ,
+    info: "das ist ein beispiel text. hier kann alles stehen. beschreibung etc. xxxx sjf erfjergner ekjg regwghwrth wrtgwthwrth wrthwtrhrwth"
+    
 }]
+
+exports.faq = [{
+    id: 1,
+    question: "Question 1",
+    answer: "Answer 1"
+},{
+    id: 2,
+    question: "Question 2",
+    answer: "Answer 2"
+},{
+    id: 3,
+    question: "Question 3",
+    answer: "Answer 3"
+},{
+    id: 4,
+    question: "Question 4",
+    answer: "Answer 4"
+}]
+
+exports.comments = [{
+    id: 1,
+    author: "Andi",
+    headline: "XXX",
+    comment: "some text ahhah"
+},{
+    id: 2,
+    author: "Bene",
+    headline: "XXX",
+    comment: "some text erubgflerlghberiug"
+},{
+    id: 3,
+    author: "cene",
+    headline: "XXX",
+    comment: "some text ehflbserhbagzuvebwh hztodiztdtizo"
+}]
+
