bcfcc053729efd050701b5c8bfad50a5bab34e6f
andilosinge <andreas.losing@gmx.de>
2022-09-17 12:15:44 +0200

    finished css

--- app.js
+++ app.js
@@ -4,59 +4,97 @@ const data = require('./data.js')
 const app = express()
 
 app.engine('hbs', expressHandlebars.engine({
-defaultLayout: 'main.hbs'
+	defaultLayout: 'main.hbs'
 }))
 
 app.use(
-    express.static('public')
+	express.static('public')
 )
- 
-app.get('/', function(request, response){
-    response.render('start.hbs')
+
+app.use(
+	express.urlencoded({
+		extended: false
+	})
+)
+
+app.get('/', function (request, response) {
+	response.render('start.hbs')
 })
 
-app.get('/sights', function(request, response){
-
-    const model = {
-        sights: data.sights
-    }
-    
-    response.render('sights.hbs', model)
+app.get('/about', function (request, response) {
+	response.render('about.hbs')
 })
 
-app.get('/faq', function(request, response){
-
-    const model = {
-        faq: data.faq
-    }
-    
-    response.render('faq.hbs', model)
+app.get('/contact', function (request, response) {
+	response.render('contact.hbs')
 })
 
-app.get('/comments', function(request, response){
-
-    const model = {
-        comments: data.comments
-    }
-    
-    response.render('comments.hbs', model)
+app.get('/sights', function (request, response) {
+	const model = {
+		sights: data.sights
+	}
+		response.render('sights.hbs', model)
 })
 
-app.get("/sights/:id", function(request, response){
-
-const id = request.params.id
-
-const sight = data.sights.find(s => s.id == id)
-
-const model= {
-    sight: sight
-}
-
-response.render('sight.hbs', model)
-
+app.get("/sights/create", function(request, response){
+	response.render("create-sight.hbs")
 })
 
+app.get('/faq', function (request, response) {
+	const model = {
+		faq: data.faq
+	}
+	response.render('faq.hbs', model)
+})
 
+app.post("/comments", function(request, response){	
+	const author = request.body.author
+	const headline = request.body.headline
+	const comment = request.body.comment
+	const rating = request.body.rating
+	data.comments.push({
+		id: data.comments.at(-1).id + 1,
+		author: author,
+		headline: headline,
+		comment: comment,
+		rating: rating
+	})	
+	response.redirect("/comments")	
+})
+
+app.get('/comments', function (request, response) {
+	const model = {
+		comments: data.comments
+	}
+	response.render('comments.hbs', model)
+})
+
+app.post("/sights/create", function(request, response){	
+	const name = request.body.name
+	const location = request.body.location
+	const info = request.body.info
+	data.sights.push({
+		id: data.sights.at(-1).id + 1,
+		name: name,
+		location: location,
+		info: info
+	})	
+	response.redirect("/sights")	
+})
+
+app.delete("/sight", function(request, response){
+	const id = request.params.id
+	
+})
+
+app.get("/sights/:id", function (request, response) {
+	const id = request.params.id
+	const sight = data.sights.find(s => s.id == id)
+	const model = {
+		sight: sight
+	}
+	response.render('sight.hbs', model)
+})
 
 app.listen(8080)
 
--- data.js
+++ data.js
@@ -1,68 +1,71 @@
 exports.sights = [{
-    id: 1, 
-    name: "Lake of Konstanz",
-    location: "Konstanz" ,
-    info: "das ist ein beispiel text. hier kann alles stehen. beschreibung etc. xxxx sjf erfjergner ekjg regwghwrth wrtgwthwrth wrthwtrhrwth"
+	id: 1,
+	name: "Lake of Konstanz",
+	location: "Konstanz",
+	info: "das ist ein beispiel text. hier kann alles stehen. beschreibung etc. xxxx sjf erfjergner ekjg regwghwrth wrtgwthwrth wrthwtrhrwth"
 },
 {
-    id: 2, 
-    name: "Brandenburger Tor",
-    location: "Berlin" ,
-    info: "das ist ein beispiel text. hier kann alles stehen. beschreibung etc. xxxx sjf erfjergner ekjg regwghwrth wrtgwthwrth wrthwtrhrwth"
-    
-},{
-    id: 3, 
-    name: "Castle Neuschwanstein",
-    location: "Schwangau", 
-    info: "das ist ein beispiel text. hier kann alles stehen. beschreibung etc. xxxx sjf erfjergner ekjg regwghwrth wrtgwthwrth wrthwtrhrwth"
+	id: 2,
+	name: "Brandenburger Tor",
+	location: "Berlin",
+	info: "das ist ein beispiel text. hier kann alles stehen. beschreibung etc. xxxx sjf erfjergner ekjg regwghwrth wrtgwthwrth wrthwtrhrwth"
+
+}, {
+	id: 3,
+	name: "Castle Neuschwanstein",
+	location: "Schwangau",
+	info: "das ist ein beispiel text. hier kann alles stehen. beschreibung etc. xxxx sjf erfjergner ekjg regwghwrth wrtgwthwrth wrthwtrhrwth"
 },
 {
-    id: 4, 
-    name: "Sight 4",
-    location: "Berlin" ,
-    info: "das ist ein beispiel text. hier kann alles stehen. beschreibung etc. xxxx sjf erfjergner ekjg regwghwrth wrtgwthwrth wrthwtrhrwth"
-    
+	id: 4,
+	name: "Sight 4",
+	location: "Berlin",
+	info: "das ist ein beispiel text. hier kann alles stehen. beschreibung etc. xxxx sjf erfjergner ekjg regwghwrth wrtgwthwrth wrthwtrhrwth"
+
 },
 {
-    id: 5, 
-    name: "Sight 5",
-    location: "Berlin" ,
-    info: "das ist ein beispiel text. hier kann alles stehen. beschreibung etc. xxxx sjf erfjergner ekjg regwghwrth wrtgwthwrth wrthwtrhrwth"
-    
+	id: 5,
+	name: "Sight 5",
+	location: "Berlin",
+	info: "das ist ein beispiel text. hier kann alles stehen. beschreibung etc. xxxx sjf erfjergner ekjg regwghwrth wrtgwthwrth wrthwtrhrwth"
+
 }]
 
 exports.faq = [{
-    id: 1,
-    question: "Question 1",
-    answer: "Answer 1"
-},{
-    id: 2,
-    question: "Question 2",
-    answer: "Answer 2"
-},{
-    id: 3,
-    question: "Question 3",
-    answer: "Answer 3"
-},{
-    id: 4,
-    question: "Question 4",
-    answer: "Answer 4"
+	id: 1,
+	question: "Question 1",
+	answer: "Answer 1"
+}, {
+	id: 2,
+	question: "Question 2",
+	answer: "Answer 2"
+}, {
+	id: 3,
+	question: "Question 3",
+	answer: "Answer 3"
+}, {
+	id: 4,
+	question: "Question 4",
+	answer: "Answer 4"
 }]
 
 exports.comments = [{
-    id: 1,
-    author: "Andi",
-    headline: "XXX",
-    comment: "some text ahhah"
-},{
-    id: 2,
-    author: "Bene",
-    headline: "XXX",
-    comment: "some text erubgflerlghberiug"
-},{
-    id: 3,
-    author: "cene",
-    headline: "XXX",
-    comment: "some text ehflbserhbagzuvebwh hztodiztdtizo"
+	id: 1,
+	author: "Andi",
+	headline: "XXX",
+	comment: "some text ahhah",
+	rating: 5
+}, {
+	id: 2,
+	author: "Bene",
+	headline: "XXX",
+	comment: "some text erubgflerlghberiug",
+	rating: 5
+}, {
+	id: 3,
+	author: "cene",
+	headline: "XXX",
+	comment: "some text ehflbserhbagzuvebwh hztodiztdtizo",
+	rating: 5
 }]
 
