9e398f24f29f21f28700ef86af4bb037ce6e50c4
andilosinge <andreas.losing@gmx.de>
2022-09-26 15:17:38 +0200

    implemented validation

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const sqlite3 = require('sqlite3')
+const expressSession = require("express-session")
 
 const db = new sqlite3.Database("topSights-database.db")
 
 
 const multer = require('multer')
 const upload = multer({storage:multer.memoryStorage()})
+
+const SIGHT_NAME_MAX_LENGTH = 40
+const SIGHT_CITY_MAX_LENGTH = 40
+const SIGHT_COUNTRY_MAX_LENGTH = 40
+const SIGHT_INFO_MAX_LENGTH = 250
+
+const FAQ_QUESTION_MAX_LENGTH = 100
+const FAQ_ANSWER_MAX_LENGTH = 250
+
+const COMMENT_AUTHOR_MAX_LENGTH = 40
+const COMMENT_TOPIC_MAX_LENGTH = 40
+const COMMENT_TEXT_MAX_LENGTH = 40
+const COMMENT_RATING_MAX_NUMBER = 10
+
+const CORRECT_ADMIN_USERNAME = "Andi"
+const CORRECT_ADMIN_PASSWORD = "123"
 
 db.run(`
 	CREATE TABLE IF NOT EXISTS sights   (
@@ -47,10 +64,35 @@ app.use(
 )
 
 app.use(
+	expressSession({
+		saveUninitialized: false,
+		resave: false,
+		secret: "laevwfezglwf"
+	})
+)
+
+
+app.use(
+	express.static('node_modules/spectre.css/dist')
+)
+
+
+
+app.use(
 	express.urlencoded({
 		extended: false
 	})
 )
+
+//eigene middleware
+
+app.use(function(request, response, next){
+	const isLoggedIn = request.session.isLoggedIn
+
+	response.locals.isLoggedIn = isLoggedIn
+
+	next()
+})
 
 app.get('/', function (request, response) {
 	response.render('start.hbs')
@@ -63,6 +105,61 @@ app.get('/about', function (request, response) {
 app.get('/contact', function (request, response) {
 	response.render('contact.hbs')
 })
+
+app.get('/login', function (request, response) {
+	response.render('login.hbs')
+})
+
+//ändern in post request -- dafür neue seite
+app.get('/logout', function (request, response) {
+	request.session.isLoggedIn = false
+	response.redirect('/')
+})
+
+app.post('/login', function(request, response){
+	const enteredUsername = request.body.username
+	const enteredPassword = request.body.password
+
+	if(CORRECT_ADMIN_USERNAME == enteredUsername && CORRECT_ADMIN_PASSWORD == enteredPassword){
+		request.session.isLoggedIn = true
+		response.redirect('/')
+	} else{
+		response.redirect('/sights')
+	// fehlerbearbeitung
+
+	}
+
+})
+
+function getValidationErrorsForSight(name, city, country, info){
+	const errorMessages = []
+	
+	if(name == ""){
+		errorMessages.push("Name can't be empty")
+	}else if(SIGHT_NAME_MAX_LENGTH < name.length){
+		errorMessages.push("Name must be shorter than "+SIGHT_NAME_MAX_LENGTH+" characters long")
+	}
+
+	if(city == ""){
+		errorMessages.push("City can't be empty")
+	}else if(SIGHT_CITY_MAX_LENGTH < city.length){
+		errorMessages.push("City must be shorter than "+SIGHT_CITY_MAX_LENGTH+" characters long")
+	}
+
+	if(country == ""){
+		errorMessages.push("Country can't be empty")
+	}else if(SIGHT_COUNTRY_MAX_LENGTH < country.length){
+		errorMessages.push("Country must be shorter than "+SIGHT_COUNTRY_MAX_LENGTH+" characters long")
+	}
+
+	if(info == ""){
+		errorMessages.push("Info of sight can't be empty")
+	}else if(SIGHT_INFO_MAX_LENGTH < info.length){
+		errorMessages.push("Info must be shorter than "+SIGHT_INFO_MAX_LENGTH+" characters long")
+	}
+
+	return errorMessages
+}
 
 app.get('/sights', function (request, response) {
 	const query = `SELECT * FROM sights`
@@ -78,128 +175,6 @@ app.get("/sights/create", function(request, response){
 	response.render("create-sight.hbs")
 })
 
-app.get('/faqs', function (request, response) {
-	const query = `SELECT * FROM faqs`
-	db.all(query, function(error, faqs){
-		const model = {
-			faqs
-		}
-		response.render('faqs.hbs', model)
-	})	
-})
-
-app.get("/faq/create", function(request, response){
-	response.render("create-faq.hbs")
-})
-
-app.post("/faq/create", function(request, response){	
-	const question = request.body.question
-	const answer = request.body.answer
-
-	const query = `INSERT INTO faqs (question, answer) VALUES (?, ?)`
-	const values = [question, answer,]
-	db.run(query, values, function(error){
-		response.redirect("/faqs")	
-	})
-})
-
-app.post("/faq/delete/:id", function(request, response){
-	const id = request.params.id
-	const query = `DELETE FROM faqs WHERE id = ?`
-	const values = [id]
-	
-	db.run(query, values, function(error){
-		response.redirect("/faqs")
-	})	
-	
-})
-
-app.get("/faq/update/:id", function (request, response) {
-	const id = request.params.id
-
-	const query = `SELECT * FROM faqs WHERE id = ?`
-	const values = [id]
-	db.get(query, values, function(error, faq){
-		const model = {
-			faq
-		}
-		response.render('update-faq.hbs', model)
-	})	
-})
-
-app.post("/faq/update/:id", function(request, response){
-	const id = request.params.id
-	const newQuestion = request.body.question
-	const newAnswer = request.body.answer
-	
-	const query = `UPDATE faqs SET question = ?, answer = ? WHERE id = ?`
-	const values = [newQuestion, newAnswer, id]
-	db.run(query, values, function(error){
-		response.redirect("/faqs")
-	})
-})
-
-app.post("/comments/create", function(request, response){	
-	const author = request.body.author
-	const topic = request.body.topic
-	const text = request.body.text
-	const rating = request.body.rating
-
-	const query = `INSERT INTO comments (author, topic, text, rating) VALUES (?, ?, ?, ?)`
-	const values = [author, topic, text, rating]
-	db.run(query, values, function(error){
-		response.redirect("/comments")	
-	})
-})
-
-app.get('/comments', function (request, response) {
-	const query = `SELECT * FROM comments`
-	db.all(query, function(error, comments){
-		const model = {
-			comments
-		}
-		response.render('comments.hbs', model)
-	})
-})
-
-app.get("/comment/update/:id", function (request, response) {
-	const id = request.params.id
-
-	const query = `SELECT * FROM comments WHERE id = ?`
-	const values = [id]
-	db.get(query, values, function(error, comment){
-		const model = {
-			comment
-		}
-		response.render('update-comment.hbs', model)
-	})	
-})
-
-app.post("/comment/update/:id", function(request, response){
-	const id = request.params.id
-	const newAuthor = request.body.author
-	const newTopic = request.body.topic
-	const newText = request.body.text
-	const newRating = request.body.rating
-
-	const query = `UPDATE comments SET author = ?, topic = ?, text = ?, rating = ? WHERE id = ?`
-	const values = [newAuthor, newTopic, newText, newRating, id]
-	db.run(query, values, function(error){
-		response.redirect("/comments")
-	})
-})
-
-app.post("/comment/delete/:id", function(request, response){
-	const id = request.params.id
-	const query = `DELETE FROM comments WHERE id = ?`
-	const values = [id]
-	
-	db.run(query, values, function(error){
-		response.redirect("/comments")
-	})	
-	
-})
-
 app.post("/sights/create", upload.single('image'), function(request, response){	
 	const name = request.body.name
 	const city = request.body.city
@@ -207,11 +182,50 @@ app.post("/sights/create", upload.single('image'), function(request, response){
 	const info = request.body.info
 	const image = request.file.buffer.toString('base64')
 
+	const errorMessages = getValidationErrorsForSight(name, city, country, info)
+	
+	if(errorMessages.length == 0){
+
 	const query = `INSERT INTO sights (name, city, country, info, image) VALUES (?, ?, ?, ?, ?)`
 	const values = [name, city, country, info, image]
 	db.run(query, values, function(error){
-		response.redirect("/sights")	
+
+		if(error){
+				
+			errorMessages.push("Internal server error")
+
+			const model = {
+				errorMessages,
+				name,
+				city,
+				country,
+				info,
+				image
+			}
+
+			response.render('create-sight.hbs', model)
+		}else{
+			response.redirect("/sights")
+		}
+
+			
 	})
+
+	} else{
+		
+		const model = {
+			errorMessages,
+			name,
+			city,
+			country,
+			info,
+			image
+		}
+
+		
+		response.render('create-sight.hbs', model)
+		
+	}
 })
 
 app.post("/sight/delete/:id", function(request, response){
@@ -245,11 +259,50 @@ app.post("/sight/update/:id", upload.single('image'), function(request, response
 	const newInfo = request.body.info
 	const newImage = request.file.buffer.toString('base64')
 
-	const query = `UPDATE sights SET name = ?, city = ?, country = ?, info = ?, image = ? WHERE id = ?`
-	const values = [newName, newCity, newCountry, newInfo, newImage, id]
-	db.run(query, values, function(error){
-		response.redirect("/sights")
-	})
+	const errorMessages = getValidationErrorsForSight(newName, newCity, newCountry, newInfo)
+
+	if(errorMessages.length == 0){
+		
+
+		const query = `UPDATE sights SET name = ?, city = ?, country = ?, info = ?, image = ? WHERE id = ?`
+		const values = [newName, newCity, newCountry, newInfo, newImage, id]
+		db.run(query, values, function(error){
+
+			if(error){
+				
+				errorMessages.push("Internal server error")
+	
+				const model = {
+					errorMessages,
+					sight: {
+						id,
+						name: newName,
+						city: newCity,
+						country: newCountry,
+						info: newInfo
+						}
+					}
+	
+				response.render('update-sight.hbs', model)
+			}else{
+				response.redirect("/sights")
+			}
+			
+		})
+	} else{
+		const model= {
+			errorMessages,
+			sight: {
+				id,
+				name: newName,
+				city: newCity,
+				country: newCountry,
+				info: newInfo
+			} 
+
+		}
+		response.render('update-sight.hbs', model)
+	}
 })
 
 app.get("/sights/:id", function (request, response) {
@@ -263,6 +316,320 @@ app.get("/sights/:id", function (request, response) {
 		response.render('sight.hbs', model)
 	})
 })
+
+app.get('/faqs', function (request, response) {
+	const query = `SELECT * FROM faqs`
+	db.all(query, function(error, faqs){
+		const model = {
+			faqs
+		}
+		response.render('faqs.hbs', model)
+	})	
+})
+
+function getValidationErrorsForFaq(question,answer){
+	const errorMessages = []
+	
+	if(question == ""){
+		errorMessages.push("Question can't be empty")
+	}else if(FAQ_QUESTION_MAX_LENGTH < question.length){
+		errorMessages.push("Question must be shorter than "+FAQ_QUESTION_MAX_LENGTH+" characters long")
+	}
+
+	if(answer == ""){
+		errorMessages.push("Answer can't be empty")
+	}else if(FAQ_ANSWER_MAX_LENGTH < answer.length){
+		errorMessages.push("Answer must be shorter than "+FAQ_ANSWER_MAX_LENGTH+" characters long")
+	}
+
+	return errorMessages
+}
+
+app.get("/faq/create", function(request, response){
+	response.render("create-faq.hbs")
+})
+
+app.post("/faq/create", function(request, response){	
+	const question = request.body.question
+	const answer = request.body.answer
+
+	const errorMessages = getValidationErrorsForFaq(question, answer)
+
+	if(errorMessages.length == 0){
+
+		const query = `INSERT INTO faqs (question, answer) VALUES (?, ?)`
+		const values = [question, answer,]
+		db.run(query, values, function(error){
+
+			if(error){
+				
+				errorMessages.push("Internal server error")
+	
+				const model = {
+					errorMessages,
+					question,
+					answer
+				}
+	
+				response.render('create-faq.hbs', model)
+			}else{
+				response.redirect("/faqs")
+			}
+	})
+	} else{
+		const model = {
+			errorMessages,
+			question,
+			answer
+		}
+
+		
+		response.render('create-faq.hbs', model)
+	}
+})
+
+app.post("/faq/delete/:id", function(request, response){
+	const id = request.params.id
+	const query = `DELETE FROM faqs WHERE id = ?`
+	const values = [id]
+	
+	db.run(query, values, function(error){
+		response.redirect("/faqs")
+	})	
+	
+})
+
+app.get("/faq/update/:id", function (request, response) {
+	const id = request.params.id
+
+	const query = `SELECT * FROM faqs WHERE id = ?`
+	const values = [id]
+	db.get(query, values, function(error, faq){
+		const model = {
+			faq
+		}
+		response.render('update-faq.hbs', model)
+	})	
+})
+
+app.post("/faq/update/:id", function(request, response){
+	const id = request.params.id
+	const newQuestion = request.body.question
+	const newAnswer = request.body.answer
+
+	const errorMessages = getValidationErrorsForFaq(newQuestion, newAnswer)
+
+	if(errorMessages.length == 0){
+	
+		const query = `UPDATE faqs SET question = ?, answer = ? WHERE id = ?`
+		const values = [newQuestion, newAnswer, id]
+		db.run(query, values, function(error){
+
+			if(error){
+					
+				errorMessages.push("Internal server error")
+
+				const model = {
+					errorMessages,
+					faq: {
+						id,
+						question: newQuestion,
+						answer: newAnswer,
+						}
+					}
+
+				response.render('update-faq.hbs', model)
+			}else{
+				response.redirect("/faqs")
+			}
+			
+		}) 
+	} else{
+		const model= {
+			errorMessages,
+					faq: {
+						id,
+						question: newQuestion,
+						answer: newAnswer,
+						}
+
+		}
+		response.render('update-faq.hbs', model)
+	}
+})
+
+function getValidationErrorsForComment(author, topic, text, rating){
+	const errorMessages = []
+
+	if(author == ""){
+		errorMessages.push("Author can't be empty")
+	}else if(COMMENT_AUTHOR_MAX_LENGTH < author.length){
+		errorMessages.push("Author must be shorter than "+COMMENT_AUTHOR_MAX_LENGTH+" characters long")
+	}
+
+	if(topic == ""){
+		errorMessages.push("Topic can't be empty")
+	}else if(COMMENT_TOPIC_MAX_LENGTH < topic.length){
+		errorMessages.push("Topic must be shorter than "+COMMENT_TOPIC_MAX_LENGTH+" characters long")
+	}
+
+	if(text == ""){
+		errorMessages.push("Text can't be empty")
+	}else if(COMMENT_TEXT_MAX_LENGTH < topic.length){
+		errorMessages.push("Text must be shorter than "+COMMENT_TEXT_MAX_LENGTH+" characters long")
+	}
+
+	if(isNaN(rating)){
+		errorMessages.push("You did not enter a number for the rating")
+	}else if(rating == ""){
+		errorMessages.push("Rating can't be empty")
+	}else if(rating < 0){
+		errorMessages.push("Rating may not be negative")
+	}else if(10 < rating){
+		errorMessages.push("Rating may at most be 10")
+	}
+
+	return errorMessages
+}
+
+app.get('/comments', function (request, response) {
+	const query = `SELECT * FROM comments`
+	db.all(query, function(error, comments){
+		const model = {
+			comments
+		}
+		response.render('comments.hbs', model)
+	})
+})
+
+app.get("/comments/create", function(request, response){
+	response.render("create-comment.hbs")
+})
+
+app.post("/comments/create", function(request, response){	
+	const author = request.body.author
+	const topic = request.body.topic
+	const text = request.body.text
+	const rating = parseInt(request.body.rating)
+
+	const errorMessages = getValidationErrorsForComment(author, topic, text, rating)
+
+	if(errorMessages.length == 0){
+
+		const query = `INSERT INTO comments (author, topic, text, rating) VALUES (?, ?, ?, ?)`
+		const values = [author, topic, text, rating]
+		db.run(query, values, function(error){
+
+			if(error){
+				
+				errorMessages.push("Internal server error")
+	
+				const model = {
+					errorMessages,
+					author,
+					topic,
+					text,
+					rating
+				}
+	
+				response.render('create-comment.hbs', model)
+			}else{
+				response.redirect("/comments")
+			}
+		})
+	} else{
+
+		const model = {
+			errorMessages,
+			author,
+			topic,
+			text,
+			rating
+		}
+
+		response.render('create-comment.hbs', model)
+
+	}
+})
+
+
+
+app.get("/comment/update/:id", function (request, response) {
+	const id = request.params.id
+
+	const query = `SELECT * FROM comments WHERE id = ?`
+	const values = [id]
+	db.get(query, values, function(error, comment){
+		const model = {
+			comment
+		}
+		response.render('update-comment.hbs', model)
+	})	
+})
+
+app.post("/comment/update/:id", function(request, response){
+	const id = request.params.id
+	const newAuthor = request.body.author
+	const newTopic = request.body.topic
+	const newText = request.body.text
+	const newRating = request.body.rating
+
+	const errorMessages = getValidationErrorsForComment(newAuthor, newTopic, newText, newRating)
+
+	if(errorMessages.length == 0){
+
+		const query = `UPDATE comments SET author = ?, topic = ?, text = ?, rating = ? WHERE id = ?`
+		const values = [newAuthor, newTopic, newText, newRating, id]
+		db.run(query, values, function(error){
+
+			if(error){
+					
+				errorMessages.push("Internal server error")
+
+				const model = {
+					errorMessages,
+					comment: {
+						id,
+						author: newAuthor,
+						topic: newTopic,
+						text: newText,
+						rating: newRating
+						}
+					}
+
+				response.render('update-comment.hbs', model)
+			}else{
+				response.redirect("/comments")
+			}
+		})
+	}else{
+		const model= {
+			errorMessages,
+			comment: {
+				id,
+				author: newAuthor,
+				topic: newTopic,
+				text: newText,
+				rating: newRating
+				}
+
+		}
+		response.render('update-comment.hbs', model)
+	}
+})
+
+app.post("/comment/delete/:id", function(request, response){
+	const id = request.params.id
+	const query = `DELETE FROM comments WHERE id = ?`
+	const values = [id]
+	
+	db.run(query, values, function(error){
+		response.redirect("/comments")
+	})	
+	
+})
+
+
 
 app.listen(8080)
 
